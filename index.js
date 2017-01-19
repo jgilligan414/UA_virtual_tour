3cb76b0e1e310702c7fdc743572d60f3f5b6c001
Jessica Summers <jessicasummers@Jessicas-MacBook-Air.local>
2017-01-19 13:21:03 -0600

    added panos and changes html and js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var modal = document.getElementById('myModal');
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+console.log(img);
 img.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+    //modalImg.src = this.src;
+    //captionText.innerHTML = this.alt;
+    modal.innerHTML = '<iframe width="1008" height="300px" allowfullscreen frameborder="0" src="vrview/index.html17_jsummers?image=images/pano2.png"></iframe>'
 }
 
 // Get the <span> element that closes the modal
@@ -25,10 +27,11 @@ var vidmodal = document.getElementById('myModalVid');
 var vid = document.getElementById('myVid');
 var modalVid = document.getElementById("vid01");
 var vidcaptionText = document.getElementById("vidcaption");
-img.onclick = function(){
-    vidmodal.style.display = "block";
-    modalVid.src = this.src;
-    vidcaptionText.innerHTML = this.alt;
+vid.onclick = function(){
+   vidmodal.style.display = "block";
+  //  modalVid.src = this.src;
+  //vidcaptionText.innerHTML = this.alt;
+  modalVid.innerHTML= '<iframe width="560" height="315" id=”marketingvideo” src="https://www.youtube.com/embed/PHjaB_6mPos" frameborder="0" allowfullscreen></iframe>';
 }
 
 // Get the <span> element that closes the modal
