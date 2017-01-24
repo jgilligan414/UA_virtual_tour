b5a6f87c822c6a672b39a6262a8956b0b31a42f0
Jessica Summers <jessicasummers@Jessicas-MacBook-Air.local>
2017-01-24 16:07:30 -0600

    added brescia, hartnett, and ohara pages

---
+++ brescia.js
@@ -0,0 +1,43 @@
+// Get the modal - 360s
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+console.log(img);
+img.onclick = function(){
+    modal.style.display = "block";
+    //modalImg.src = this.src;
+    //captionText.innerHTML = this.alt;
+    modalImg.innerHTML = '<iframe width="1008" height="300px" allowfullscreen frameborder="0" src="vrview/index.html?image=images/pano6.png"></iframe>'
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// Get the modal - videos
+var vidmodal = document.getElementById('myModalVid');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var vid = document.getElementById('myVid');
+var modalVid = document.getElementById("vid01");
+var vidcaptionText = document.getElementById("vidcaption");
+vid.onclick = function(){
+   vidmodal.style.display = "block";
+  //  modalVid.src = this.src;
+  //vidcaptionText.innerHTML = this.alt;
+  modalVid.innerHTML= '<iframe width="560" height="315" src="https://www.youtube.com/embed/f6x1jJpOYWA" frameborder="0" allowfullscreen></iframe>';
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+//span.onclick = function() {
+  //vidmodal.style.display = "none";
+//}
